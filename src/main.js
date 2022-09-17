3b62f056946e39fa0c8bd6798faf5c4302aabee3
wang.weiran <wang.wei.ran@outlook.com>
2022-09-18 04:34:28 +1000

    npm install md5-js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import axios from "axios"
 
+Vue.prototype.axios = axios
 Vue.config.productionTip = false
 
 new Vue({
